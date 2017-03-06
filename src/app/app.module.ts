b1031f21e883fb37185d6426202e7015e2ea963c
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-03-06 21:15:01 +0900

    feat(project): create initial clarity based set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ClarityModule } from 'clarity-angular';
 
 import { AppComponent } from './app.component';
+import { PomodoroRunnerModule } from './pomodoro-runner/pomodoro-runner.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ClarityModule.forRoot(),
     FormsModule,
-    HttpModule
+    HttpModule,
+    PomodoroRunnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
