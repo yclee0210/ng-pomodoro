e2fb887ef77f3d897e8515f47863e64659f8fafd
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-03-20 11:37:03 +0900

    test(tasks.service): add missing spec
    https://app.asana.com/0/287748850352677/294233534395425

--- src/app/pomodoro/tasks.service.spec.ts
+++ src/app/pomodoro/tasks.service.spec.ts
@@ -1,16 +1,49 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { TasksService } from './tasks.service';
+import {TestBed, async, inject} from '@angular/core/testing';
+import {TasksService} from './tasks.service';
+import {StoreModule, Store} from '@ngrx/store';
+import {TasksActions} from '../_reducers/tasks.actions';
+
+class MockStore {
+  public dispatch(obj) {
+  }
+
+  public select(key) {
+
+    return key;
+  }
+}
 
 describe('TasksService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TasksService]
+      providers: [
+        TasksService,
+        {provide: Store, useClass: MockStore}
+      ]
     });
   });
 
-  it('should ...', inject([TasksService], (service: TasksService) => {
+  it('should define', inject([TasksService, Store], (service: TasksService) => {
     expect(service).toBeTruthy();
+    expect(service.tasks$).toBeDefined();
   }));
+
+  describe('method: createAndSelect', () => {
+    it('should dispatch create and select with input name string', inject(
+      [TasksService, Store],
+      (service: TasksService, store: Store<any>) => {
+        const name = 'New task';
+        spyOn(store, 'dispatch');
+
+        service.createAndSelect(name);
+
+        expect(store.dispatch).toHaveBeenCalledWith({
+          type: TasksActions.CREATE_AND_SELECT_TASK,
+          payload: name
+        });
+      }
+    ));
+  });
 });
