326d296d7b0644601e313e4edf9c1086da7a4385
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-03-20 10:45:17 +0900

    test(task-select.service): add missing spec
    https://app.asana.com/0/287748850352677/294233534395425

--- src/app/pomodoro/task-select.service.spec.ts
+++ src/app/pomodoro/task-select.service.spec.ts
@@ -1,16 +1,96 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { TaskSelectService } from './task-select.service';
+import {TestBed, async, inject} from '@angular/core/testing';
+import {TaskSelectService} from './task-select.service';
+import {Store} from '@ngrx/store';
+import {Task} from '../_domains/task';
+import {TaskState} from '../_enums/task-state.enum';
+import {ObjectState} from '../_enums/object-state.enum';
+import {TaskSelectActions} from '../_reducers/task-select.actions';
+
+class MockStore {
+  public dispatch(obj) {
+  }
+
+  public select(key) {
+
+    return key;
+  }
+}
 
 describe('TaskSelectService', () => {
+  let testFactory: TasksTestFactory;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TaskSelectService]
+      providers: [
+        TaskSelectService,
+        {provide: Store, useClass: MockStore}
+      ]
     });
+    testFactory = new TasksTestFactory();
   });
 
-  it('should ...', inject([TaskSelectService], (service: TaskSelectService) => {
+  afterEach(() => {
+    testFactory = undefined;
+  });
+
+  it('should be defined', inject([TaskSelectService, Store], (service: TaskSelectService, store: Store<any>) => {
     expect(service).toBeTruthy();
+    expect(service.selectedTask$).toEqual('selectedTask');
   }));
+
+  describe('method: select', () => {
+    it('should dispatch SELECT with input', inject(
+      [TaskSelectService, Store],
+      (service: TaskSelectService, store: Store<any>) => {
+        spyOn(store, 'dispatch');
+        const task = testFactory.getTestTask(1);
+
+        service.select(task);
+
+        expect(store.dispatch).toHaveBeenCalledWith({
+          type: TaskSelectActions.SELECT,
+          payload: task
+        });
+      }
+    ));
+  });
+
+  describe('method: deselect', () => {
+    it('should dispatch SELECT with input', inject(
+      [TaskSelectService, Store],
+      (service: TaskSelectService, store: Store<any>) => {
+        spyOn(store, 'dispatch');
+
+        service.deselect();
+
+        expect(store.dispatch).toHaveBeenCalledWith({
+          type: TaskSelectActions.DESELECT
+        });
+      }
+    ));
+  });
 });
+
+class TasksTestFactory {
+  getTestTask(id: number): Task {
+
+    return {
+      id: id,
+      name: 'Test Task',
+      pomodoros: [],
+      taskState: TaskState.UNTOUCHED,
+      start: null,
+      end: null,
+      state: ObjectState.ACTIVE
+    };
+  }
+
+  getTestTasks(): Task[] {
+
+    return [
+      this.getTestTask(1),
+      this.getTestTask(2)
+    ]
+  }
+}
